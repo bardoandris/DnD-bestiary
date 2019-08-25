8f88600c67052c46f57408c3ce02017fafc7e6e2
bardoandris <bardoandris@users.noreply.github.com>
2019-08-25 17:43:35 +0200

    Fixed the bullshit

--- test.php
+++ test.php
@@ -1,11 +1,22 @@
 <?php
-$filename = $_GET["Creaturename"] + ".json";
+$filename = $_GET["CreatureName"] . ".json";
 strtolower($filename);
-$filepath = "JSON/" + $filename;
-if (file_exists($filename)) {
+$filepath = "JSON/" . $filename;
+if (file_exists($filepath)) {
+    $json = file_get_contents($filepath);
+    echo('<a href="index.html" <h1>Back</h1> </a> <br>');
+    writeresponse($json);
     
 }
 else {
-    echo("FileDoesntExist");
+    echo('<a href="index.html" <h1>Back</h1></a> <br>');
+    echo("FileDoesntExist");    
+}
+function writeresponse($rawjson){
+     $decodedjson = json_decode($rawjson, true);
+     $imgpath ='img/'. strtolower($decodedjson['CreatureName']);
+     for ($i=1; $i <= $decodedjson['PageCount']; $i++) { 
+         echo(' <img src="' . $imgpath . $i .'.jpg" >');
+     }
 }
 ?>
